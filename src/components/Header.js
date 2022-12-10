dbd49752fa4dfef1cf0f1ca1ca14d21ddfb4c91e
Graham Patton <graham.m.patton@gmail.com>
2022-12-09 23:46:49 -0500

    got rid of add button on about page by adding location in header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,36 @@
 import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
 import Button from './Button'
 
 const Header = ({ title, onAdd, showAdd }) => {
-    
-    return (
-        <header className='header'>
-        <h1>{title}</h1>
-        <Button 
-            color={showAdd ? 'darkblue' : 'green'}
-            text={showAdd ? 'Close' : 'Add'}
-            onClick={onAdd} 
-            />
-        </header>
-    )
+  const location = useLocation()
+
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      {location.pathname === '/' && (
+        <Button
+          color={showAdd ? 'red' : 'green'}
+          text={showAdd ? 'Close' : 'Add'}
+          onClick={onAdd}
+        />
+      )}
+    </header>
+  )
 }
 
 Header.defaultProps = {
-    title: 'Task Tracker',
+  title: 'Task Tracker',
 }
 
 Header.propTypes = {
-    title: PropTypes.string,
+  title: PropTypes.string.isRequired,
 }
 
-// CSS in js
+// CSS in JS
 // const headingStyle = {
-//     color:'blue', 
-//     backgroundColor: 'black'
+//   color: 'red',
+//   backgroundColor: 'black',
 // }
 
 export default Header
